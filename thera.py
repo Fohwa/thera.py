5fdf07d653c92bc150d0594dc05f85d5a79d9570
Fohwa <78933542+Fohwa@users.noreply.github.com>
2021-05-17 22:07:05 +0200

    of course the numbers have to go from 1-9

--- thera.py
+++ thera.py
@@ -9,19 +9,19 @@ while 1:
      print("Fuck you, idiot!")
  elif therapy == 2:
      print("Have you tried alcohol?")
- elif therapy == 2:
+ elif therapy == 3:
      print("Aha, I have the feeling you feel alone")
- elif therapy == 2:
+ elif therapy == 4:
      print("Mmmmh, I dont care")
- elif therapy == 2:
+ elif therapy == 5:
      print("Is this really the way you feel?")
- elif therapy == 2:
+ elif therapy == 6:
      print("I am the opinion you are insane")
- elif therapy == 2:
+ elif therapy == 7:
      print("This is the way!")
- elif therapy == 2:
+ elif therapy == 8:
      print("yah, no")
- elif therapy == 2:
+ elif therapy == 9:
      print("actually this is 100% true")
  else:
      print("Whatever, I cant help you!")
