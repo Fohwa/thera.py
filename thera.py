41c936491eff3c72f06b82064b2e9627feace2db
Fohwa <78933542+Fohwa@users.noreply.github.com>
2021-05-17 21:50:01 +0200

    first commit

---
+++ thera.py
@@ -0,0 +1,27 @@
+import random
+
+while 1:
+ statement = input("What is your problem: ")
+ 
+ therapy = random.randrange(1, 10)
+ 
+ if therapy == 1:
+     print("Fuck you, idiot!")
+ elif therapy == 2:
+     print("Have you tried alcohol?")
+ elif therapy == 2:
+     print("Aha, I have the feeling you feel alone")
+ elif therapy == 2:
+     print("Mmmmh, I dont care")
+ elif therapy == 2:
+     print("Is this really the way you feel?")
+ elif therapy == 2:
+     print("I am the opinion you are insane")
+ elif therapy == 2:
+     print("This is the way!")
+ elif therapy == 2:
+     print("yah, no")
+ elif therapy == 2:
+     print("actually this is 100% true")
+ else:
+     print("Whatever, I cant help you!")
