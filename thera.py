5abd22abaf2016d3fe765c6f21868afaf1b9cdc0
Fohwa <md.fohwa@gmail.com>
2021-05-18 12:46:52 +0200

    cleaned up the code: using array instead of if statements. makes the code clean and scales better.

--- thera.py
+++ thera.py
@@ -1,56 +1,43 @@
 import random
 
+#the idea is to generate bools and categories. The bools score for the individual categories and the one with highest score will be chossen
+# Alternative could be choosen to use lines with words missing, that will be added by another category, but this will be difficult
+
+class category:
+   linecount = 10
+
+   line = ["Fuck you, idiot!", "Have you tried alcohol?",
+   "Aha, I have the feeling you feel alone", "Mmmmh, I dont care",
+   "Is this really the way you feel?", "I am the opinion you are insane",
+   "This is the way!", "yeah, no", "actually this is 100% true",
+   "Whatever, I cant help you!"]
+
+normal = category()
+
 while 1:
  statement = input("What is your problem: ")
 
  if "help" in statement:
-    therapy = random.randrange(1, 5)
-    if therapy == 1:
-        print("Nobody can help you")
-    elif therapy == 2:
-        print("I will not help you")
-    elif therapy == 3:
-        print("Are you weak? You need help?")
-    elif therapy == 4:
-        print("Help is just an excuse")
-    elif therapy == 5:
-        print("I need help, too. Will you help me?")
+    i = random.randrange(1, 5)
+    help = category()
+    help.line = ["Nobody can help you",
+    "I will not help you", "Are you weak? You need help?",
+    "Help is just an excuse", "I need help, too. Will you help me?", ]
+
+    print(help.line[i])
+    
+    
  elif "hello" in statement or "hey" in statement or "hi" in statement:
-    therapy = random.randrange(1, 5)
-    if therapy == 1:
-        print("You can go away right away!")
-    elif therapy == 2:
-        print("Fuck off, nobody likes you")
-    elif therapy == 3:
-        print("Hey, just kiding, I dont like you!")
-    elif therapy == 4:
-        print("Hello and bye")
-    elif therapy == 5:
-        print("yeah, lets skip over the samll talk part")
+
+    i = random.randrange(1, 5)
+    hi = category()
+    hi.line = ["You can go away right away!", "Fuck off, nobody likes you",
+    "Hey, just kiding, I dont like you!", "Hello and bye",
+    "yeah, lets skip over the samll talk part", ]
+    
+    print(hi.line[i])
 
  else:
-  therapy = random.randrange(1, 10)
- 
-  if therapy == 1:
-     print("Fuck you, idiot!")
-  elif therapy == 2:
-     print("Have you tried alcohol?")
-  elif therapy == 3:
-     print("Aha, I have the feeling you feel alone")
-  elif therapy == 4:
-     print("Mmmmh, I dont care")
-  elif therapy == 5:
-     print("Is this really the way you feel?")
-  elif therapy == 6:
-     print("I am the opinion you are insane")
-  elif therapy == 7:
-     print("This is the way!")
-  elif therapy == 8:
-     print("yah, no")
-  elif therapy == 9:
-     print("actually this is 100% true")
-  else:
-     print("Whatever, I cant help you!")
+  i = random.randrange(1, 10)
 
-therapy = 0
-statement = ""
+  print(normal.line[i])
