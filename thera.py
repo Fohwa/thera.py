86b08eba2dd75835b7761353de6c972fbec95d0b
Fohwa <md.fohwa@gmail.com>
2021-05-18 20:52:45 +0200

    text to speech for therapist

--- thera.py
+++ thera.py
@@ -1,7 +1,22 @@
 import random
+import pyttsx3 #for text to speech to make it more terrible
 
-#the idea is to generate bools and categories. The bools score for the individual categories and the one with highest score will be chossen
+# the idea is to generate bools and categories. The bools score for the individual categories and the one with highest score will be chossen
 # Alternative could be choosen to use lines with words missing, that will be added by another category, but this will be difficult
+
+def speak(x):
+
+   #initialise
+   engine = pyttsx3.init()
+
+   #sets a voice
+   voices = engine.getProperty('voices') 
+   engine.setProperty('voice', voices[1].id)
+
+   #actual speaking
+   engine.say(x)
+   engine.runAndWait()
+
 
 class category:
    linecount = 10
@@ -25,6 +40,8 @@ while 1:
     "Help is just an excuse", "I need help, too. Will you help me?", ]
 
     print(help.line[i])
+    speak(help.line[i])
+
     
     
  elif "hello" in statement or "hey" in statement or "hi" in statement:
@@ -36,8 +53,12 @@ while 1:
     "yeah, lets skip over the samll talk part", ]
     
     print(hi.line[i])
+    speak(hi.line[i])
 
  else:
   i = random.randrange(1, 10)
 
   print(normal.line[i])
+  speak(normal.line[i])
+
+
